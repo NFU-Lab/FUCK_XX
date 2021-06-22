459d883b015df139ee42dfc9eaff0487c7ddfdcc
johnpoint <me@lvcshu.com>
2021-06-22 17:45:55 +0800

    注册登录基础

---
+++ api/controller/login.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+用户登录页面
+*/
+
+define('ACC', true);
+require('./include/init.php');
+
+
+$u = $_POST['username'];
+$p = $_POST['passwd'];
+
+//合法性检测
+
+//核对用户名密码
+$user = new UserModel();
+$row = $user->checkUser($u, $p);
+if (empty($row)) {
+	$resp["msg"] = '用户名密码错误';
+	$resp["code"] = 403;
+} else {
+	$resp["msg"] = '登陆成功';
+	$resp["code"] = 200;
+	$_SESSION["FPHPX"] = true;
+	session_start();
+	$_SESSION = $row;
+}
+
+echo $resp;
--- api/index.php
+++ api/index.php
@@ -1,1 +1,35 @@
 <?php
+/*
+路由文件，通过识别路由进行不同的控制器处理
+*/
+require('./include/init.php');
+header("Content-type:application/json");
+$session = $_SESSION["FPHPX"];
+$router = $_POST("router"); // 获取当前的路由
+
+// 身份校验
+if (!($router == "login" || $router == "register")) {
+    if (!(isset($_SESSION["FPHPX"]) && $_SESSION["FPHPX"] === true)) {
+        $_SESSION["FPHPX"] = false;
+        $resp["code"] = 403;
+        $resp["msg"] = "Forbidden";
+        exit;
+    }
+}
+
+// 处理路由
+switch ($router) {
+    case 'index':
+        require_once("./controller/index.php");
+        break;
+    case 'login':
+        require_once("./controller/login.php");
+        break;
+    case 'register':
+        require_once("./controller/regAct.php");
+    default:
+        $resp["code"] = 404;
+        $resp["msg"] = "Page Not Found";
+        echo $resp;
+        break;
+}
