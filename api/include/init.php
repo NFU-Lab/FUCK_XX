80d6bead7f4eb2aa8073a616cb02697556a03200
johnpoint <me@lvcshu.com>
2021-06-27 11:47:55 +0800

    1

--- api/include/init.php
+++ api/include/init.php
@@ -3,7 +3,8 @@
 作用:框架初始化
 */
 
-header('Access-Control-Allow-Origin:*'); // *代表允许任何网址请求
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+header('Access-Control-Allow-Origin:' . $origin); // *代表允许任何网址请求
 header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE'); // 允许请求的类型
 header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
 header('Access-Control-Allow-Headers: Content-Type,Content-Length,Accept-Encoding,X-Requested-with, Origin'); // 设置允许自定义请求头的字段
