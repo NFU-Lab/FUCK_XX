17feddd28e4d7013b3f26078909e84d5951cc53e
johnpoint <me@lvcshu.com>
2021-06-27 15:48:07 +0800

    1

--- api/controller/change_checkout_status.php
+++ api/controller/change_checkout_status.php
@@ -7,17 +7,17 @@ $c = $_POST["class"];
 $s = $_POST["status"];
 $d = $_POST["date"];
 
-$r = DB::table('status')->where(array('user' => $u, 'class' => $c, 'date' => $d))->first();
+$r = DB::table('status')->where(array('stuno' => $u, 'class' => $c, 'date' => $d))->first();
 
 try {
     if (empty($r)) {
-        if (DB::table('status')->insert(array('user' => $u, 'class' => $c, 'date' => $d, 'status' => $s))) {
+        if (DB::table('status')->insert(array('stuno' => $u, 'class' => $c, 'date' => $d, 'status' => $s))) {
             exit(successResponse());
         } else {
             exit(failResponse("change status failed"));
         }
     } else {
-        if (DB::table('status')->update(array('status' => $s, 'date' => $d))) {
+        if (DB::table('status')->update(array('stuno' => $u, 'status' => $s, 'date' => $d))) {
             exit(successResponse());
         } else {
             exit(failResponse("change status failed"));
